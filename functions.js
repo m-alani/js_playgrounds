f026db61eeee75dd3fa0c46afd24902e4831d38d
Marwan Alani <marwan@alani.ca>
2017-04-19 16:07:00 -0400

    Playing around with functions

---
+++ functions.js
@@ -0,0 +1,21 @@
+let myFunction = function (text = "Nothing") {
+  console.log(text);
+}
+
+myFunction(); // Nothing
+
+myFunction('Passed parameter'); // Passed parameter
+
+function defaultText() {
+  return "No text";
+}
+myFunction = function (text = defaultText()) { // excuted only if no parameter is passed
+  console.log(text);
+}
+
+myFunction(); // No text
+
+console.log(myFunction); // [Function: myFunction]
+
+let sum = (arg1, arg2) => arg1 + arg2;
+console.log(sum(3,11)); // 14
